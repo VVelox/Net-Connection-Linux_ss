cebdac3952d1a0f84e7822592e1319324749529e
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-07-31 20:46:22 -0500

    add initial files

---
+++ lib/Net/Connection/Linux_ss.pm
@@ -0,0 +1,105 @@
+package Net::Connection::Linux_ss;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Net::Connection::Linux_ss - The great new Net::Connection::Linux_ss!
+
+=head1 VERSION
+
+Version 0.0.1
+
+=cut
+
+our $VERSION = '0.0.1';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Connection::Linux_ss;
+
+    my $foo = Net::Connection::Linux_ss->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-connection-linux_ss at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Connection-Linux_ss>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Connection::Linux_ss
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Connection-Linux_ss>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Net-Connection-Linux_ss>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Net-Connection-Linux_ss>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2026 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+
+=cut
+
+1; # End of Net::Connection::Linux_ss
